d1adabd4b9039223e925c94e2c6509fc386e1258
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-15 21:24:25 +0100

    bootstrap haz lo que quieras

--- src/components/Containers/Footer/Footer.js
+++ src/components/Containers/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Footer.css'
 
 
+
 const Footer = () => {
 
     /*     const address = "South East,London";
@@ -16,18 +17,6 @@ const Footer = () => {
                 <div class="row">
                     <h6>About</h6>
                     <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
-                </div>
-
-                <div class="col-xs-6 col-md-3">
-                    <h6>Categories</h6>
-                    <ul class="footer-links">
-                        <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-                        <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-                        <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-                        <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-                        <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-                        <li><a href="http://scanfcode.com/category/templates/">Templates</a></li> */}
-            </ul>
                 </div>
 
                 <div class="col-xs-6 col-md-3">
