51c540d01117af81ab108312d44d8771a10de931
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-15 16:12:09 +0100

    footer example done

--- src/components/Containers/Footer/Footer.js
+++ src/components/Containers/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Footer.css'
 
+
 const Footer = () => {
 
     /*     const address = "South East,London";
@@ -13,10 +14,8 @@ const Footer = () => {
         <footer class="site-footer">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-12 col-md-6">
-                        <h6>About</h6>
-                        <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
-                    </div>
+                    <h6>About</h6>
+                    <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
                 </div>
 
                 <div class="col-xs-6 col-md-3">
@@ -27,10 +26,9 @@ const Footer = () => {
                         <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
                         <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
                         <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-                        <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
-                    </ul>
+                        <li><a href="http://scanfcode.com/category/templates/">Templates</a></li> */}
+            </ul>
                 </div>
-
 
                 <div class="col-xs-6 col-md-3">
                     <h6>Quick Links</h6>
@@ -39,16 +37,17 @@ const Footer = () => {
                         <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
                         <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
                         <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-                        <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
-                    </ul>
+                        <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li> */}
+            </ul>
                 </div>
-
-                    <div class="row">
-                        <div class="col-md-8 col-sm-6 col-xs-12">
-                            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
+            </div>
+            <hr />
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-6 col-xs-12">
+                        <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
          <a href="#">Scanfcode</a>.
             </p>
-                        </div>
                     </div>
 
                     <div class="col-md-4 col-sm-6 col-xs-12">
@@ -56,10 +55,11 @@ const Footer = () => {
                             <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
                             <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
                             <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                        </ul>
+                            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   */}
+            </ul>
                     </div>
                 </div>
+            </div>
         </footer>
     )
 }
