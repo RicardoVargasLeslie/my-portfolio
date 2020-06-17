bd44a487965f8952fabbd8d2301dfa1f964366a8
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-17 06:31:32 +0100

    final commit

--- src/components/Me/Me.js
+++ src/components/Me/Me.js
@@ -12,7 +12,7 @@ const Me = ({ children }) => {
     return (
         <div id="me">
             {children}
-            <Avatar avatarInfo={avatarInfo}></Avatar>
+         
         </div>
     )
 }
