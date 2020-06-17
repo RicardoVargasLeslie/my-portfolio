d8cf4ed1c0d8f22332d261e65036768e1f223d94
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-17 06:12:38 +0100

    yes its 6 am and im comiting

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -2,9 +2,16 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => (
-  <header id="header">
-    <div>
-    <h3>HEADER</h3>
+
+  <header class="site-header">
+    <div className="contaimer">
+    <h1 class="title">Space for your headline.</h1>
+    <h2 class="subtitle">A space for a more descriptive subtitle.</h2>
+    <p class="disclaimer">A small space for a disclaimer.</p>
+    <button class="get-started button button-primary button-large">Call to Action</button>
+
+    <a class="scroll-down" href="#"></a>
+
     </div>
   </header>
 )
