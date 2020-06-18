43268fb7e8ab5df7c0daded643ad2080426b6eb9
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-18 16:49:25 +0100

    header texto estatico

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -8,14 +8,18 @@ const Header = () => (
 
       <div id="parent">
 
-        <p className="animation">Hi there I'm Ricki a software developer based
+      <h2>Hi there I'm Ricki</h2>
+        <p className="animation">a software developer based
          in London.
+         <br></br>
        
         If you where expecting one of those fancy portfolios with high-resolution images and too many animations that all look the same ,
         y regret to disappoint you but that's not me.
+        <br></br>
         
         I write code yes, but most of it run under the hood,  therefore you can't see it,
         I could show you some snippets or you could go to my Github profile and take a peek.
+        <br></br>
          Don't forget less is more :)
          </p>
       </div>
