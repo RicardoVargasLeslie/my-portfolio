3f4ca29839327d6aa192abedef4aff2fabfdbe69
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-18 01:42:10 +0100

    header animation

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -6,16 +6,22 @@ const Header = () => (
   <header class="site-header">
     <div className="contaimer">
 
-    <div id="parent">This is a typewriter effect.<div id="border"></div></div>
+      <div id="parent">
+     
+      <p>"Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+      fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident,
+       sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div>
 
-
-
-    
-    <button class="get-started button button-primary button-large">Call to Action</button>
-
-    <a class="scroll-down" href="#"></a>
 
     </div>
+    <button class="get-started button button-primary button-large">Call to Action</button>
   </header>
 )
 
