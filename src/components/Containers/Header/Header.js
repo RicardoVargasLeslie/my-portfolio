095b5062f41a1d8c999421bb5ac90676ef4fb36e
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-22 15:40:50 +0100

    lo que sea

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import ReactTypingEffect from 'react-typing-effect';
 
 const Header = () => (
 
@@ -8,25 +9,34 @@ const Header = () => (
 
       <div id="parent">
 
-      <h2>Hi there I'm Ricki</h2>
-        <p className="animation">a software developer based
-         in London.
-         <br></br>
-       
-        If you where expecting one of those fancy portfolios with high-resolution images and too many animations that all look the same ,
-        y regret to disappoint you but that's not me.
-        <br></br>
-        
-        I write code yes, but most of it run under the hood,  therefore you can't see it,
-        I could show you some snippets or you could go to my Github profile and take a peek.
-        <br></br>
-         Don't forget less is more :)
-         </p>
+        <h2>Hi there I'm Ricki</h2>
+
+        <ReactTypingEffect
+
+          text="a software developer based
+          in London..
+
+      If you where expecting one of those fancy portfolios with high-resolution images and too many animations that all look the same ,
+      y regret to disappoint you but that's not me.
+
+      I write code yes, but most of it run under the hood,  therefore you can't see it,
+      I could show you some snippets or you could go to my Github profile and take a peek.
+      "
+      
+
+       spped="1000000"
+    
+        />
+
       </div>
-
-
+      <ul class="social-header">
+        <li><a href="https://github.com/RicardoVargasLeslie" class="icon-github">GitHub</a></li>
+      </ul>
+      <p className="animation"> Don't forget less is more :)</p>
+      <br></br>
     </div>
-    <button>Take me to Github</button>
+
+
   </header>
 )
 
