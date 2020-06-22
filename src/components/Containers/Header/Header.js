bba7b2ba3eadeb3bf72557e4b1e3bd5eceb7a4d7
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-22 16:31:37 +0100

    nice text

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Header.css'
-import ReactTypingEffect from 'react-typing-effect';
+import Typewriter from 'typewriter-effect';
 
 const Header = () => (
+
+
+
 
   <header class="site-header">
     <div className="contaimer">
@@ -10,25 +13,41 @@ const Header = () => (
       <div id="parent">
 
         <h2>Hi there I'm Ricki</h2>
+      </div>
+
+      <Typewriter
+        options={{
+          strings: ['a software developer based in london','If you where expecting one of those fancy portfolios with high-resolution images and too many animations that all look the same ,',' y regret to disappoint you but thats not me.'],
+          autoStart: true,
+          loop: true,
+          changeDeleteSpeed:0,
+          
+        }}
+      />
+
+
+      {/*   <ReactTypingEffect
+
+          text="a software developer based
+          in London..."
+          speed="50"
+        /> */}
+      {/*  <ReactTypingEffect
+
+          text="If you where expecting one of those fancy portfolios with high-resolution images and too many animations that all look the same ,
+          y regret to disappoint you but that's not me."
+          speed="50"
+          cursor=""
+          eraseDelay="0"
+        />
 
         <ReactTypingEffect
 
-          text="a software developer based
-          in London..
-
-      If you where expecting one of those fancy portfolios with high-resolution images and too many animations that all look the same ,
-      y regret to disappoint you but that's not me.
-
-      I write code yes, but most of it run under the hood,  therefore you can't see it,
-      I could show you some snippets or you could go to my Github profile and take a peek.
-      "
-      
-
-       spped="1000000"
-    
-        />
-
-      </div>
+          text="I write code yes, but most of it run under the hood,  therefore you can't see it,
+           I could show you some snippets or you could go to my Github profile and take a peek. "                                                           
+          speed="50"
+          cursor=""
+        /> */}
       <ul class="social-header">
         <li><a href="https://github.com/RicardoVargasLeslie" class="icon-github">GitHub</a></li>
       </ul>
