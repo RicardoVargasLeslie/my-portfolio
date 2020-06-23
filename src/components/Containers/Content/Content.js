abb242aabda22cd9cbb56c7b651bea2e6fccd181
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-23 15:54:47 +0100

    fixes

--- src/components/Containers/Content/Content.js
+++ src/components/Containers/Content/Content.js
@@ -3,32 +3,29 @@ import './Content.css'
 import Me from '../../Me/Me'
 import Work from '../../Work/Work'
 import Contact from "../../Contact/Contact"
-import  Card from '../../Card/Card'
+import Card from '../../Shared/Card/Card'
 import contactAvatar from '../../../assets/images/avatar2.png'
 import meAvatar from '../../../assets/images/117-ironman.png'
 import workAvatar from '../../../assets/images/avatar3.png'
 
 
-const Content = () => {
+const Content = () => (
 
-    return (
-        <div id="content">
-            <Me>
-             
-             <Card img={meAvatar} trigger={ <p>me</p> }></Card>
-        
-            </Me>
-            <Work>
-            <Card img={workAvatar} trigger={ <p>work</p> }></Card>
-            </Work>
-            <Contact>
-            <Card img={contactAvatar} trigger={ <p>contact</p> }></Card>
-            </Contact>
+    <div id="content">
+        <Me>
 
+            <Card img={meAvatar} trigger={<p>me</p>}></Card>
+
+        </Me>
+        <Work>
+            <Card img={workAvatar} trigger={<p>work</p>}></Card>
+        </Work>
+        <Contact>
+            <Card img={contactAvatar} trigger={<p>contact</p>}></Card>
+        </Contact>
 
 
-        </div>
-    )
-}
+
+    </div>)
 
 export default Content
--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -13,18 +13,17 @@ const Header = () => (
 
       <Typewriter
         options={{
-          strings: ['a software developer based in london','If you where expecting a fancy portfolios with high-resolution images,','and too many animations,' ,'y regret to disappoint you but thats not me'],
+          strings: ['a software developer based in london', 'If you where expecting a fancy portfolios with high-resolution images,', 'and too many animations,', 'y regret to disappoint you but thats not me'],
           autoStart: true,
           loop: true,
-          delay:31
+          delay: 31
         }}
       />
       <ul class="social-header">
         <li><a href="https://github.com/RicardoVargasLeslie" class="icon-github">GitHub</a></li>
       </ul>
-      <br></br>
-    </div>
 
+    </div>
 
   </header>
 )
