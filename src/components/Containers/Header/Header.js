729623106058b77a85bd28b06ec04eca180e3296
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-06-27 23:59:27 +0100

    top

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import Typewriter from "typewriter-effect";
 
 
 
@@ -8,6 +9,23 @@ const Header = () => (
     <div className="containerheader">
       <div id="parent">
         <h1> {'{ welcome: "Hi there I\'m Ricki" }'}</h1>
+      </div>
+
+      <div className="letras">
+        {<Typewriter
+          options={{
+            strings: [
+              "a software developer based in london",
+              "If you where expecting a fancy portfolios with high-resolution images,",
+              "and too many animations,",
+              "y regret to disappoint you but thats not me",
+            ],
+            autoStart: true,
+            loop: false,
+            delay: 31,
+            cursor: ""
+          }}
+        />}
       </div>
     </div>
 
