f051bc41d7b24d0e213ec0eb093116181a382228
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-15 17:25:23 +0100

    added styled component footer

--- src/components/Containers/Footer/Footer.js
+++ src/components/Containers/Footer/Footer.js
@@ -1,51 +1,129 @@
 import React from "react";
-import "./Footer.css";
 import Social from "./../../Shared/Social/Social";
-import FooterStyled from "./FooterStyled";
+import styled from "styled-components";
+
+const SiteFooter = styled.div`
+
+* {
+  align-items: baseline;
+  align-items: baseline;
+  vertical-align: baseline;
+  align-self: baseline;
+}
+
+  background-color: #424242;
+  padding: 5px;
+  font-size: 15px;
+  line-height: 24px;
+  color: #424242;
+  text-align: center;
+  margin-top: 5%;
+
+  hr {
+
+     border-top-color: #bbb;
+     opacity: 0.5;
+     border-top-color: #79787c;
+    
+  }
+
+  h6 {
+
+  color: #fff;
+  font-size: 16px;
+  text-transform: uppercase;
+  margin-top: 5px;
+  letter-spacing: 2px;
+  font-family: "Roboto", sans-serif;
+  }
+`;
+
+const FooterLinks = styled.div`
+ 
+  list-style: none;
+  display: inline-block;
+  text-align: center;
+  margin-top: 0%;
+
+  a{
+  display: inline-block;
+  text-align: center;
+  color: #79787c;
+  }
+
+  li
+  {
+     display: inline-block;
+     text-align: center;
+  }
+
+  a:active,a:focus,a:hover 
+  {
+    color: #ffc107;
+    text-decoration: none;
+  }
+`;
+
+const AboutContainer = styled.div`
+ color: #ffc107;
+`;
+const CopyrightContainer = styled.div`
+ color: #79787c;
+ margin: 0;
+ text-align: center;
+`;
+
 
 const Footer = () => {
     return (
-        <footer class="site-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="about-container">
-                        <h6>About</h6>
-                        <p>{'{ about: "I\'m Just a Developer" }'}</p>
-                    </div>
-                    <div class="quick-container">
+        <SiteFooter>
+            <div>
+                <div>
+                    <AboutContainer>
+                        <div>
+                            <h6>About</h6>
+                            <p>{'{ about: "I\'m Just a Developer" }'}</p>
+                        </div>
+                    </AboutContainer>
+                    <div>
                         <h6>Quick Links</h6>
-                        <ul class="footer-links">
-                            <li>
-                                <a href="http://scanfcode.com/about/">About Us</a>
-                            </li>
-                            <li>
-                                <a href="http://scanfcode.com/contact/">Contact Us</a>
-                            </li>
-                            <li>
-                                <a href="http://scanfcode.com/privacy-policy/">
-                                    Privacy Policy
+                        <FooterLinks>
+                            <ul>
+                                <li>
+                                    <a href="http://scanfcode.com/about/">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="http://scanfcode.com/contact/">Contact Us</a>
+                                </li>
+                                <li>
+                                    <a href="http://scanfcode.com/privacy-policy/">
+                                        Privacy Policy
                 </a>
-                            </li>
-                            <li>
-                                <a href="http://scanfcode.com/sitemap/">Sitemap</a>
-                            </li>
-                        </ul>
+                                </li>
+                                <li>
+                                    <a href="http://scanfcode.com/sitemap/">Sitemap</a>
+                                </li>
+                            </ul>
+                        </FooterLinks>
                     </div>
                 </div>
                 <hr />
             </div>
-            <div class="copyright-container">
+
+            <CopyrightContainer>
                 <div>
                     <div>
-                        <Social></Social>
-                        <p>
+                        <div>
+                            <Social></Social>
+                            <p>
 
-                            {'{ cp: " Copyright 2020 All Rights Reserved by Ricardo Vargas" }'}
-                        </p>
+                                {'{ cp: " Copyright 2020 All Rights Reserved by Ricardo Vargas" }'}
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </footer>
+            </CopyrightContainer>
+        </SiteFooter>
     );
 };
 
