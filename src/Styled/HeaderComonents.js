b643dea32d7f6b8bd8105bfffc0ef839bcd46ac5
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-18 13:51:10 +0100

    added all styles to header component

--- src/Styled/HeaderComonents.js
+++ src/Styled/HeaderComonents.js
@@ -10,6 +10,14 @@ export const SiteHeader = styled.div`
   width: auto;
   height: 60%;
   padding: 10px;
+
+
+  * {
+  align-items: baseline;
+  vertical-align: baseline;
+  align-self: baseline;
+}
+
 `;
 
 export const Parent = styled.div`
@@ -21,4 +29,22 @@ export const Parent = styled.div`
   font-family: "Roboto", sans-serif;
   color: #424242;
   font-size: 25px;
+`;
+
+export const Parent = styled.div`
+ 
+   text-align: center;
+  margin-top: 10%;
+  white-space: nowrap;
+  overflow: hidden;
+  font-family: "Roboto", sans-serif;
+  color: #424242;
+  font-size: 25px;
+`;
+
+export const ContainerHeader = styled.div`
+ 
+   font-family: "Roboto", sans-serif;
+  color: #000000;
+  font-size: 22px;
 `;
