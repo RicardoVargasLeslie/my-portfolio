f7fe09518c705bb272ef189eea1f3dcc08a2bd2f
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-19 09:47:41 +0100

    added all sub componenets to avatar

--- src/components/Containers/Content/MeContent/Avatar/Avatar.js
+++ src/components/Containers/Content/MeContent/Avatar/Avatar.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./Avatar.css";
 import avatar from "../../../../../assets/images/avatar.jpg";
 
-import { ContactHolder, ContactImageHolder, Content, Bottom } from "../../../../../Styled/AvatarComponents"
+import { ContactHolder, ContactImageHolder, Content, Bottom } from "../../../../../Styled/AvatarComponents";
 
 function Avatar(props) {
     return (
         <div className="container">
-            <div class="contact-holder">
-                <div class="contact-image-holder">
+            <ContactHolder>
+                <ContactImageHolder>
                     <img src={avatar} />
-                </div>
-                <div class="bottom">
+                </ContactImageHolder>
+                <Bottom>
                     <h6>or 1=1</h6>
-                </div>
-            </div>
+                </Bottom>
+            </ContactHolder>
         </div>
     );
 }
