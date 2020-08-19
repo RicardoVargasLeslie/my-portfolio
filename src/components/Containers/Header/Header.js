fe22d49b00a7ac5201afcc5de87f68e116a80488
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-19 09:50:52 +0100

    rolled back header due to errors

--- src/components/Containers/Header/Header.js
+++ src/components/Containers/Header/Header.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./Header.css";
 import TypeWriter from "react-typewriter";
-import { SiteHeader, Parent, ContainerHeader } from "../../../Styled/FooterComponents"
+//import { SiteHeader, Parent, ContainerHeader } from "../../../Styled/FooterComponents"
 
 
 
 const Header = () => (
-  <SiteHeader>
-    <header class="site-header">
-      <div className="containerheader">
-        <div id="parent">
-          <h1> {'{ welcome: "Hi there I\'m Ricki" }'}</h1>
-        </div>
-        <TypeWriter typing={1}>{'{ Iam: "A software developer based in london " }'}</TypeWriter>
 
+  <header class="site-header">
+    <div className="containerheader">
+      <div id="parent">
+        <h1> {'{ welcome: "Hi there I\'m Ricki" }'}</h1>
       </div>
+      <TypeWriter typing={1}>{'{ Iam: "A software developer based in london " }'}</TypeWriter>
 
-    </header >
-  </SiteHeader>
+    </div>
+
+  </header >
+
 
 
 );
