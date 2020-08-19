a9ec71c4aca9be52f781dc0c79cc8834a3a88640
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-08-19 09:37:00 +0100

    created vatarComponents style

---
+++ src/Styled/AvatarComponents.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ContactHolder = styled.div`
+
+ text-align: center;
+  width: 50%;
+  margin: 0 auto;
+  padding: 20px;
+  margin: 100px auto 0px;
+  background: #424242;
+  border-radius: 10px;
+  box-shadow: 0px 0px 9px rgba(0, 0, 0, 0.7), 0px 4px 0px #424242,
+    inset 0px 1px 0px #fff, inset 0px -1px 0px rgba(0, 0, 0, 0.4),
+    0px 6px 26px rgba(0, 0, 0, 0.4);
+ 
+`;
